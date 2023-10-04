829e67417ad18cc3dd89c601d7480123a6a0503d
Chisom Chukwuma <chukwumachisom18@gmail.com>
2023-10-04 13:40:10 +0100

    handling null and undefined

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { IProduct } from './product';
 
 @Component({
   // the selector property is only required when the component will be nested
@@ -8,6 +9,9 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
+  // the property(product) has a type(IProduct) interface or undefined. undefined because we may not get the data immediately because of http and TS strong typing won't allow us initialise the property later(they may if we use definite assignment assertion though(product!) but we cannot go with this alternative since our data may take time to get)
+  // the data is undefined until the data is received, then it'll be of IProduct data type
+  product: IProduct | undefined;
 
   // reads the route parameter so it knows which product detail to display
   // the ActivatedRoute service provides info on the state of the current route, including the route params
@@ -20,6 +24,16 @@ export class ProductDetailComponent implements OnInit {
     // we add the Number function so that the params('id') becomes a number data type
     const id = Number(this.route.snapshot.paramMap.get('id'));
     // to see the Id we read from the Url, we display it as part of the pageTitle
-    this.pageTitle = this.pageTitle + `: ${id}`;
+    this.pageTitle = this.pageTitle + ` ${id}`;
+    this.product = {
+      productId: id,
+      productName: 'Saw',
+      productCode: 'TBX-0022',
+      releaseDate: 'May 15, 2021',
+      description: '15-inch steel blade hand saw',
+      price: 11.55,
+      starRating: 3.7,
+      imageUrl: 'assets/images/saw.png',
+    };
   }
 }
