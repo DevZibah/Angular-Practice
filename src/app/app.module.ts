76ac324c6a8eb555093e56b4778d6ef8be42ccd6
Chisom Chukwuma <chukwumachisom18@gmail.com>
2023-10-04 21:05:39 +0100

    created the ProductModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star.component';
-import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
 import { RouterModule } from '@angular/router';
-import { productDetailGuard } from './products/product-detail.guard';
 import { ProductModule } from './products/product.module';
 
 @NgModule({
   // used for declaring components, directives and pipes that belongs to this module
-  declarations: [
-    AppComponent,
-    WelcomeComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    ProductDetailComponent,
-  ],
+  declarations: [AppComponent, WelcomeComponent],
   // for pulling in external modules
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
     HttpClientModule,
     // passing an array of routes
     RouterModule.forRoot([
-      { path: 'products', component: ProductListComponent },
-      // the CanActivate guard prevents navigation to he product-detail route if the id is 0 or NaN(not a number)
-      {
-        path: 'products/:id',
-        // here we specify each guard we want to execute when activating the route
-        canActivate: [productDetailGuard],
-        component: ProductDetailComponent,
-      },
       { path: 'welcome', component: WelcomeComponent },
       // default route
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,12 +1,34 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { ProductListComponent } from './product-list.component';
+import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
+import { ProductDetailComponent } from './product-detail.component';
+import { StarComponent } from '../shared/star.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { productDetailGuard } from './product-detail.guard';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProductListComponent,
+    ProductDetailComponent,
+    ConvertToSpacesPipe,
+    StarComponent,
+  ],
   imports: [
-    CommonModule
-  ]
+    // the CommonModule gives access to ngFor and ngIf directives
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([
+      { path: 'products', component: ProductListComponent },
+      // the CanActivate guard prevents navigation to he product-detail route if the id is 0 or NaN(not a number)
+      {
+        path: 'products/:id',
+        // here we specify each guard we want to execute when activating the route
+        canActivate: [productDetailGuard],
+        component: ProductDetailComponent,
+      },
+    ]),
+  ],
 })
-export class ProductModule { }
+export class ProductModule {}
